6a7b34c82138432aedfe3591e3adc7a01f06ddaa
Luc cole <luccoleb@yahoo.com>
2022-04-26 21:30:38 -0400

    commit

--- src/api/index.js
+++ src/api/index.js
@@ -1,36 +1,30 @@
 
-// ----- WARNING! commented this out !WARNING -----
-
-// export const BASE_URL = 'https://strangers-things.herokuapp.com/api/';
-// export const COHORT_NAME = '2104-ECE-RM-WEB-PT/';
-// export const API_URL = BASE_URL + COHORT_NAME;
-
-export const API_URL = 'http://localhost:3001/api/';
+export const API_URL = 'https://fsa-fitness-tracker.herokuapp.com/api/';
 
 export const callApi = async ({ url, method, token, body }) => {
   
-    try {
-        const options = {
-            method: method ? method.toUpperCase() : 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(body),
-        };
+  try {
+    const options = {
+      method: method ? method.toUpperCase() : 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body),
+    };
 
-        if (token) {
-            options.headers['Authorization'] = `Bearer ${token}`;
-        }
-
-        const response = await fetch(API_URL + url, options);
-        const data = await response.json();
-
-        if (data.error) {
-            throw data.error;
-        }
-
-        return data;
-    } catch (error) {
-        return error;
+    if (token) {
+      options.headers['Authorization'] = `Bearer ${token}`;
     }
+
+    const response = await fetch(API_URL + url, options);
+    const data = await response.json();
+
+    if (data.error) {
+      throw data.error;
+    }
+
+    return data;
+  } catch (error) {
+    return error;
+  }
 };
